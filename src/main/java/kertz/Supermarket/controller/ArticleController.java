4052b4d8bdf5bceae7332c865d865b3bc20e55fe
k3r7z <brunoadanleanza@gmail.com>
2023-11-02 20:33:44 -0300

    Updated article controller

--- src/main/java/kertz/Supermarket/controller/ArticleController.java
+++ src/main/java/kertz/Supermarket/controller/ArticleController.java
@@ -5,10 +5,10 @@ import kertz.Supermarket.model.Article;
 import kertz.Supermarket.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ArticleController {
     private ArticleRepository repository;
 
@@ -17,23 +17,37 @@ public class ArticleController {
         this.repository = repository;
     }
 
-    @GetMapping("articles")
+    @GetMapping("/articles")
     public List<Article> getArticles(){
         return repository.findAll();
     }
 
-    @PostMapping("articles")
+    @PostMapping("/articles")
     public Article newArticle(@RequestBody Article newArticle){
         return repository.save(newArticle);
     }
 
-    @GetMapping("articles/{id}")
-    Article getArticle(@PathVariable Integer id){
+    @PutMapping("/articles/{id}")
+    public Article updateArticle(@PathVariable int id, @RequestBody Article article){
+        Article updateArticle = repository.findById(id).orElseThrow( () -> new ArticleNotFoundException(id) );
+
+        updateArticle.setDescription(article.getDescription());
+        updateArticle.setPrice(article.getPrice());
+        updateArticle.setIVA(article.getIVA());
+        updateArticle.setStock(article.getStock());
+
+        repository.save(updateArticle);
+
+        return updateArticle;
+    }
+
+    @GetMapping("/articles/{id}")
+    Article getArticle(@PathVariable int id){
         return repository.findById(id).orElseThrow( () -> new ArticleNotFoundException(id));
     }
 
-    @DeleteMapping("articles/{id}")
-    void deleteArticle(@PathVariable Integer id){
+    @DeleteMapping("/articles/{id}")
+    void deleteArticle(@PathVariable int id){
         repository.deleteById(id);
     }
 }
