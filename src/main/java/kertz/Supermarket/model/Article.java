97b246e339ce5c27d4c1933feca65a7580199ba6
kertz <brunoleanza@protonmail.com>
2023-12-14 14:04:01 -0300

    Changed article model

--- src/main/java/kertz/Supermarket/model/Article.java
+++ src/main/java/kertz/Supermarket/model/Article.java
@@ -1,6 +1,5 @@
 package kertz.Supermarket.model;
 
-
 import jakarta.persistence.*;
 
 @Entity
@@ -8,46 +7,54 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int code;
-    private float price;
     private String description;
-    private float iva;
+    private float wholesalerPrice;
+    private int gainPercentage;
+    private int ivaPercentage;
     private int stock;
 
     public Article(){}
-    public Article(int code, String description, float price, float iva, int stock) {
+    public Article(int code, String description, float wholesalerPrice, int gainPercentage, int ivaPercentage, int stock) {
         this.code = code;
         this.description = description;
-        this.price = price;
-        this.iva = iva;
+        this.wholesalerPrice = wholesalerPrice;
+        this.gainPercentage = gainPercentage;
+        this.ivaPercentage = ivaPercentage;
         this.stock = stock;
     }
 
     public int getCode() { return code; }
 
-    public float getPrice() { return price; }
-
     public String getDescription() { return description; }
 
-    public float getIVA(){ return iva; }
+    public float getWholeSalerPrice() { return wholesalerPrice; }
+
+    public int getGainPercentage() { return gainPercentage; }
+
+    public int getIVA(){ return ivaPercentage; }
+
+    public float getGrossPrice() { return wholesalerPrice + wholesalerPrice * gainPercentage / 100; }
+
+    public float getNetPrice() {
+        final float grossPrice = getGrossPrice();
+        return grossPrice + grossPrice * ivaPercentage / 100;
+    }
 
     public int getStock() { return stock; }
 
     public void setDescription(String description){ this.description = description; }
 
-    public void setPrice(float price){ this.price = price; }
+    public void setWholeSalerPrice(float wholesalerPrice){ this.wholesalerPrice = wholesalerPrice; }
 
-    public void setIVA(float iva){ this.iva = iva; }
+    public void setIVAPercentage(int iva){ this.ivaPercentage = iva; }
+
+    public void setGainPercentage(int gain){ this.gainPercentage = gain;}
 
     public void setStock(int stock) { this.stock = stock; }
 
     @Override
     public boolean equals(Object anotherArticle){
         if(anotherArticle == null || anotherArticle.getClass() != this.getClass() ) return false;
-        return this.code ==  ((Article)anotherArticle).code;
-    }
-
-    @Override
-    public String toString(){
-        return "Article [code = " + code + ", description = " + description + ", price = " + price + ", iva = " + iva + ", stock = " + stock + "]";
+        return this.code == ((Article)anotherArticle).code;
     }
 }
